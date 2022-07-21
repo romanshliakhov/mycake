eaacdf5516ac0a2bc4cfda330f393748e2525f11
Roman <romanshliakhov@gmail.com>
2022-07-22 00:44:28 +0300

    added first section

--- source/js/components/sliders.js
+++ source/js/components/sliders.js
@@ -1,51 +1,27 @@
 import Swiper from '../vendor/swiper.js';
 
-// // Dynamic row
-let sliderDynamicRow = new Swiper(".sale__slider", {
-  slidesPerView: "auto",
-  spaceBetween: 60,
-  loop: true,
-  observer: true,
-  speed: 20000,
-  autoplay: {
-      delay: 0,
-  },
-  breakpoints: {
-    320: {
-      spaceBetween: 15,
-    },
-    576: {
-      spaceBetween: 15,
-    },
-    769: {
-      spaceBetween: 60,
-    }
-}
-});
-
-
-// // Article slider
-let sliderArticle = new Swiper(".more__slider", {
-  slidesPerView: 3,
-  spaceBetween: 40,
+// Products slider
+let sliderArticle = new Swiper(".products__slider", {
+  slidesPerView: 5,
+  spaceBetween: 20,
   loop: true,
   navigation: {
-      nextEl: ".more__arrow--next",
-      prevEl: ".more__arrow--prev",
+      nextEl: ".products__slider-btn--next",
+      prevEl: ".products__slider-btn--prev",
       clickable: true,
   },
-  breakpoints: {
-      320: {
-          slidesPerView: 1,
-          spaceBetween: 20,
-        },
-      576: {
-        slidesPerView: 2,
-        spaceBetween: 50,
-      },
-      768: {
-        slidesPerView: 3,
-        spaceBetween: 40,
-      }
-  }
+  // breakpoints: {
+  //     320: {
+  //         slidesPerView: 1,
+  //         spaceBetween: 20,
+  //       },
+  //     576: {
+  //       slidesPerView: 2,
+  //       spaceBetween: 50,
+  //     },
+  //     768: {
+  //       slidesPerView: 3,
+  //       spaceBetween: 40,
+  //     }
+  // }
 });
