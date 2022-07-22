81a14a17c17d2824332d0fb2b4e0585f01f152e7
Roman <romanshliakhov@gmail.com>
2022-07-22 18:29:52 +0300

    added burger btn animation

--- source/js/components/burger.js
+++ source/js/components/burger.js
@@ -1,14 +1,15 @@
 // burger
-let menuBtn = document.querySelector('.mobile__header-btn');
-let menuBurgerBtn = document.querySelector('.burger-menu');
-let menu = document.querySelector('.mobile__menu');
+let menuBtn = document.querySelector('.burger-menu');
+// let menu = document.querySelector('.mobile__menu');
 let disableScroll = document.querySelector('body');
 
 if (menuBtn) {
   menuBtn.addEventListener('click', function(){
     menuBtn.classList.toggle('active');
-    menuBurgerBtn.classList.toggle('active');
     menu.classList.toggle('active');
     disableScroll.classList.toggle('body-scroll');
   });
 }
+
+
+
