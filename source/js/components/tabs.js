a9dae256d19b4301602ca72e40e0781214b23619
Roman <romanshliakhov@gmail.com>
2022-07-28 18:50:38 +0300

    add confectional page

--- source/js/components/tabs.js
+++ source/js/components/tabs.js
@@ -1,30 +1,43 @@
 // tabs
 
-function tabsInit() {
-  const tabsParrents = [...document.querySelectorAll('.tabs')];
+const tabsParrents = [...document?.querySelectorAll('.tabs')];
+const tabsParrentsInner = [...document?.querySelectorAll('.tabs__inner')];
 
-  tabsParrents.map(function(tabsParr) {
+let tabBtn = 'tabs-btn', // default btn
+    tabContent = 'tabs-content', // default content
+    innerTabBtn = 'inner-btn',
+    innerTabContent = 'inner-content';
+
+function tabsInit(tabsParrentItem, btns, contents) {
+  tabsParrentItem.map(function(tabsParr) {
     if (tabsParr) {
       document.addEventListener('DOMContentLoaded', () => {
-        const tabsBtn = tabsParr.querySelectorAll('.tablinks');
-        const tabsContent = tabsParr.querySelectorAll('.tabcontent');
+        const tabsBtn = tabsParr.querySelectorAll(`[${btns}]`);
+        const tabsContent = tabsParr.querySelectorAll(`[${contents}]`);
 
-        tabsParr.addEventListener('click', (e) => {
-            if (e.target.classList.contains('tablinks')) {
-                const tabsPath = e.target.getAttribute('tabs-btn');
-                tabsBtn.forEach(el => {el.classList.remove('active')});
-                tabsParr.querySelector(`[tabs-btn="${tabsPath}"]`).classList.add('active');
-                tabsHandler(tabsPath);
-            }
-        });
+        for (const button of tabsBtn) {
+          button.addEventListener('click', function () {
+            const tabsPath = this.getAttribute(`${btns}`);
+
+            tabsPath == 'confectioner-products' ?
+            document.querySelector('.details__aside')?.classList.add('active') :
+            document.querySelector('.details__aside')?.classList.remove('active');
+
+
+            tabsBtn.forEach(el => {el.classList.remove('active')});
+            tabsParr.querySelector(`[${btns}="${tabsPath}"]`).classList.add('active');
+            tabsHandler(tabsPath);
+          });
+        }
 
         const tabsHandler = (path) => {
           tabsContent.forEach(el => {el.classList.remove('active')});
-          tabsParr.querySelector(`[tabs-content="${path}"]`).classList.add('active');
+          tabsParr.querySelector(`[${contents}="${path}"]`).classList.add('active');
         };
       });
     }}
   );
 }
 
-tabsInit();
+tabsInit(tabsParrents, tabBtn, tabContent);
+tabsInit(tabsParrentsInner, innerTabBtn, innerTabContent);
