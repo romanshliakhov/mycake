4efd82852350628dd4049caeff0899e06cea4f64
Roman <romanshliakhov@gmail.com>
2022-09-01 03:38:26 +0300

    adaptive version 1

--- source/js/components/imagesblock.js
+++ source/js/components/imagesblock.js
@@ -6,6 +6,8 @@ function setHeaderHeight() {
     let header = document.querySelector('.first__wrapper-left'),
         headerHeight = header && header.clientHeight + 'px';
     block && block.style.setProperty('--images-max-height', headerHeight)
+
+    console.log(headerHeight);
 }
 
 window.addEventListener("DOMContentLoaded", () => {
